5162ddb269961d5ad7dd7344bf5fb5c7fc9dac4b
Ralph Mallett <rmallett12@gmail.com>
2020-07-16 15:32:37 +0100

    Add journey call and methods

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,2 +1,23 @@
 class Journey
+
+  def initialize(entry_station = nil)
+    @entry_station = entry_station
+  end
+
+  def start
+    @entry_station
+  end
+
+  def end(exit_station)
+    @exit_station = exit_station
+  end
+
+  def complete?
+    @entry_station != nil && @exit_station != nil
+  end
+
+  def fare
+    complete? ? 1 : 6
+  end
+
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,5 +1,45 @@
 require 'journey'
 
 describe Journey do
+  let(:entry_station){ double :station }
+  let(:exit_station){ double :station }
 
+  it 'can start a journey' do
+    expect(subject).to respond_to(:start)
+  end
+  it 'can log the starting station' do
+    journey = Journey.new(entry_station)
+    expect(journey.start).to eq entry_station
+  end
+  it 'can log the ending station' do
+    journey = Journey.new(entry_station)
+    expect(journey.end(exit_station)).to eq exit_station
+  end
+  it 'can log a complete journey' do
+    journey = Journey.new(entry_station)
+    journey.start
+    journey.end(exit_station)
+    expect(journey.complete?).to be_truthy
+  end
+  it 'can log an incomplete journey when not ended' do
+    journey = Journey.new(entry_station)
+    journey.start
+    expect(journey.complete?).to be_falsey
+  end
+  it 'can log an incomplete journey when not started' do
+    journey = Journey.new
+    journey.end(exit_station)
+    expect(journey.complete?).to be_falsey
+  end
+  it 'can calculate the fare for a completed journey' do
+    journey = Journey.new(entry_station)
+    journey.start
+    journey.end(exit_station)
+    expect(journey.fare).to eq(1)
+  end
+  it 'can calculate the fare for an incomplete journey' do
+    journey = Journey.new
+    journey.end(exit_station)
+    expect(journey.fare).to eq(6)
+  end
 end
